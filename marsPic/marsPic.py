cda9dd730aadda70c38fff0978a0df19554b27dd
alexamarki <amarkin2006@gmail.com>
2023-03-24 10:25:35 +0300

    adspace

--- marsPic/marsPic.py
+++ marsPic/marsPic.py
@@ -21,7 +21,7 @@ def promotion():
     return '</br>'.join(list1)
 @app.route('/image_mars')
 def image():
-    return f'''<h1>Жди нас, Марс!</h1><img src="static/Unknown.jpeg"
+    return f'''<h1>Жди нас, Марс!</h1><img src="static/Mars.png"
            alt="здесь должна была быть картинка, но не нашлась"><br><span>Вот она какая, красная планета.<span>'''
 
 if __name__ == '__main__':
